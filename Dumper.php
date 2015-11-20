247d8820e73f5860db258f24cb47dd651a915e6e
Anton <toh1napocht@yandex.ru>
2015-11-21 01:12:04 +0200

    add special codes

--- Dumper.php
+++ Dumper.php
@@ -108,8 +108,17 @@ class Dumper
     }
 
     public function openDump($table_name, $table_dump){
+        $sql ="/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;\n\n";
         $table_name = $this->getTableName($table_name);
-        $sql = $table_dump."\n\n";
+        $sql .= $table_dump."\n";
+        $sql .= "LOCK TABLES `$table_name` WRITE;\n\n";
         $sql .= "INSERT INTO `$table_name` VALUES ";
 
         $file_name = $this->getFileName($table_name);
@@ -155,7 +164,16 @@ class Dumper
     }
 
     public function closeDump(){
-        fwrite($this->_fp, ";");
+        $sql = ";\n";
+        $sql .= "UNLOCK TABLES;\n\n";
+        $sql .= "/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */";
+        fwrite($this->_fp, $sql);
         fclose($this->_fp);
         unset($this->_fp);
     }
